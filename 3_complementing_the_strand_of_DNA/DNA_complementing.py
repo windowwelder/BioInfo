3bb3a13550d394140dc1913995599e254564dc19
windowwelder <welderules@duck.com>
2025-11-13 15:13:00 +0700

    initial: 3rd rosalind challenge

---
+++ 3_complementing_the_strand_of_DNA/DNA_complementing.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+data = (Path("data.txt").parent / "data" / "data.txt").read_text(encoding="utf-8")
+
+print({data.translate(str.maketrans({"A":"T", "T": "A", "G": "C", "C": "G"}))})
